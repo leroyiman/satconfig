5750d27e6658e74f7f6f73218e8e5aa4fea9c387
Leroy Macartney <leroy.macartney@googlemail.com>
2026-04-28 15:40:17 +0200

    Style admin login page with conf-hero header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # Devise-Login-Seite (sessions#new) bekommt das eigene Auth-Layout
+  # mit dem conf-hero Header. Andere Devise-Aktionen bleiben unverändert.
+  layout :resolve_layout
+
+  private
+
+  def resolve_layout
+    if devise_controller? && controller_name == "sessions" && action_name == "new"
+      "auth"
+    else
+      "application"
+    end
+  end
 end
